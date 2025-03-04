954206da2d8b9128d20ef28e9c9a8a6abdc391a9
Timotej <timi.kumin@gmail.com>
2025-03-04 15:04:00 +0100

    Koncano torek

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,24 +1,37 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { useEffect, useState } from 'react';
 
 const Card = ({title}) => {
+  const [hasLiked, setHasLiked]  = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  });
 
   return(
-  <div>
+  <div className="card">
   <h2>{title}</h2>
+
+  <button onClick={() => setHasLiked(!hasLiked)}>
+    
+    {hasLiked ? '❤️️' : '🤍'}
+    
+    </button>
+
   </div>
   )
 }
 
 const App = () => {
 
+
+
   return (
     <div className="card-container">
   
-    <Card title="Star Wars" rating={5} isCool={true} actors={[{name : "Actors" }]}/>  
+    <Card title="Star Wars" rating={5} isCool={true} />  
     <Card title="Avatar" />  
     <Card title="American Psycho"/>  
     </div>
